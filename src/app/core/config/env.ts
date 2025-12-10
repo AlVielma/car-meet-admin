be370e2a622999ed06095676078c7ed3c9cf7d4c
AlVielma <vielma7220@gmail.com>
2025-12-10 05:12:29 -0600

    changes

--- src/app/core/config/env.ts
+++ src/app/core/config/env.ts
@@ -1,3 +1,5 @@
 export const ENV = {
-  apiBaseUrl: 'https://api.carlosduron.online/api', // apunta a tu Express
+  // apiBaseUrl: 'https://api.carlosduron.online/api', // apunta a tu Express
+  apiBaseUrl: 'https://87bp7xx8-3000.usw3.devtunnels.ms/api', // apunta a tu Express
+
 };
